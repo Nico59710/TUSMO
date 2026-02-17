ebe48786d9b20960dafe610c9492f1e340fab4e5
unknown <yannmonory@gmail.com>
2026-02-17 14:16:45 +0100

    MAJ JS & CSS

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const bouttons = document.querySelectorAll(".button");
     const cells = document.querySelectorAll(".cell");
     
-    for (let index = 0; index < bouttons.length; index++) {
-        const element = bouttons[index];
-        element.addEventListener("click", function () {
-            console.log(element.textContent);
-            for (let j = 0; j < cells.length; j++) {
-                const cell = cells[j];
-                cell.textContent = element.textContent;
-            }
-        });
-    }
+   
+    let currentIndex = 0;
+
+for (let index = 0; index < bouttons.length; index++) {
+    const element = bouttons[index];
+
+    element.addEventListener("click", function () {
+
+        if (currentIndex < cells.length) {
+            cells[currentIndex].textContent = element.textContent;
+            currentIndex++;
+        }
+
+    });
+}
 
 
 
